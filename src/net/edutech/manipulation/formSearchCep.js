752f7c462b51748ea7a84070b7e0334192e1504e
Hugo <hugo_paim@estudante.sesisenai.org.br>
2026-03-01 00:47:14 -0300

    fix: corrige envio de dados do json

--- src/net/edutech/manipulation/formSearchCep.js
+++ src/net/edutech/manipulation/formSearchCep.js
@@ -7,14 +7,18 @@ const city = document.querySelector(".city");
 const federativeUnit = document.querySelector(".federativeUnit");
 
 
-export async function inputCep(){
+export async function inputCep() {
 
-    enrollmentCep.addEventListener("focusout", event =>{
-        
+    enrollmentCep.addEventListener("focusout", async event => {
+
         let cepInput = event.target.value;
-        const cepData = searchCep(cepInput);        
+        const cepData = await searchCep(cepInput);
         console.log(cepData);
-        
+
+        street.value = cepData.logradouro;
+        neighborhood.value = cepData.bairro;
+        city.value = cepData.localidade;
+        federativeUnit.value = cepData.estado;
 
     });
 }
